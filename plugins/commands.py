ea32cda79849a21e4835e5b0acc22cb38716a080
TamilVenthan Mec <85955814+darkmanrandy@users.noreply.github.com>
2021-08-03 14:37:05 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -22,7 +22,7 @@ async def start(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/TroJanzHEX")
+                            "🧐 JOIN OUR CHANNEL 😎", url="https://telegram.me/Tharamaanamovies")
                     ]
                 ]
             ),
@@ -45,7 +45,7 @@ async def help(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "⭕️ SUPPORT ⭕️", url="https://t.me/TroJanzSupport")
+                            "🧐 JOIN OUR CHANNEL 😎", url="https://telegram.me/tendkotta")
                     ]
                 ]
             ),
@@ -68,7 +68,7 @@ async def about(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "SOURCE CODE", url="https://github.com/TroJanzHEX/Auto-Filter-Bot-V2")
+                            "🧐 Join Our Team 😎", url="https://telegram.me/tharamaanateambot")
                     ]
                 ]
             ),
